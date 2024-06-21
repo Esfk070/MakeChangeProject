fa84bb98e9f116fc0ba739233a73f91d2673b029
Kevin <esfk070@gmail.com>
2024-06-21 17:15:19 -0600

    Create and close Scanner in main

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,10 +1,21 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) 
 	{
+		//TODO - create scanner
+		//TODO - pront for inputs
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		
 
+		
+		keyboard.close();
+		
 	}
 
 }
